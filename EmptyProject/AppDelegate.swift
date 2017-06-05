9c58d50de8e181aeda6ac32fa33d96c96bfb74f1
文优 <wenyou.ly@taobao.com>
2017-06-05 18:07:22 +0800

    udpate

--- EmptyProject/AppDelegate.swift
+++ EmptyProject/AppDelegate.swift
@@ -42,6 +42,23 @@ let aclPerms = [
     ACLPerm(perm: ACL_CHANGE_OWNER, name: "chown", flags: ACL_PERM_FILE | ACL_PERM_DIR)
 ]
 
+struct ACLFlag {
+    let flag: acl_flag_t
+    let name: String
+    let flags: Int
+}
+
+let aclFlags = [
+    ACLFlag(flag: ACL_ENTRY_FILE_INHERIT, name: "file_inherit", flags: ACL_PERM_DIR),
+    ACLFlag(flag: ACL_ENTRY_DIRECTORY_INHERIT, name: "directory_inherit", flags: ACL_PERM_DIR),
+    ACLFlag(flag: ACL_ENTRY_LIMIT_INHERIT, name: "limit_inherit", flags: ACL_PERM_FILE | ACL_PERM_DIR),
+    ACLFlag(flag: ACL_ENTRY_ONLY_INHERIT, name: "only_inherit", flags: ACL_PERM_DIR),
+
+    ACLFlag(flag: ACL_FLAG_NO_INHERIT, name: "??", flags: ACL_PERM_FILE | ACL_PERM_DIR),
+    ACLFlag(flag: ACL_ENTRY_INHERITED, name: "??", flags: ACL_PERM_FILE | ACL_PERM_DIR),
+    ACLFlag(flag: ACL_FLAG_DEFER_INHERIT, name: "??", flags: ACL_PERM_FILE | ACL_PERM_DIR)
+]
+
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
     static let windowSize = NSMakeSize(800, 500)
@@ -83,6 +100,13 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 //
 //        var acl : acl_perm_t
         let result = acl_get_file(url.path, ACL_TYPE_EXTENDED)
+        let fileType = ACL_PERM_FILE
+
+        // out result
+        var len = acl_size(result)
+        let s = String.init(utf8String: &acl_to_text(result, &len).pointee)
+        NSLog(s! + "\n===== =====")
+
 
         var entry: acl_entry_t?
         var id = ACL_FIRST_ENTRY
@@ -92,64 +116,87 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // sizeof https://stackoverflow.com/questions/24662864/swift-how-to-use-sizeof
         while acl_get_entry(result, id.rawValue, &entry) == 0 {
             id = ACL_NEXT_ENTRY
-            var set: acl_permset_t?
-            if acl_get_permset(entry, &set) != 0 {
-                continue
-            }
 
-            var tag: acl_tag_t = ACL_UNDEFINED_TAG
-            if acl_get_tag_type(entry, &tag) != 0 {
+            var tagT: acl_tag_t = ACL_UNDEFINED_TAG
+            if acl_get_tag_type(entry, &tagT) != 0 {
                 continue
             }
-            var tagType = ""
-            switch tag {
+            var tag = ""
+            switch tagT {
             case ACL_EXTENDED_ALLOW:
-                tagType = "allow"
+                tag = "allow"
             case ACL_EXTENDED_DENY:
-                tagType = "deny"
+                tag = "deny"
             default:
-                tagType = "unknow"
+                tag = "unknow"
             }
 
-            var permset: acl_permset_t?
-            if acl_get_permset(entry, &permset) != 0 {
+            var permsetT: acl_permset_t?
+            if acl_get_permset(entry, &permsetT) != 0 {
                 continue
             }
             aclPerms.forEach({ (aclPerm) in
-                if acl_get_perm_np(permset, aclPerm.perm) == 0 {
+                if acl_get_perm_np(permsetT, aclPerm.perm) == 0 {
                     return
                 }
-                if ((aclPerm.flags == 0 ? 1 : 0) & ACL_PERM_FILE) == 1 {
+                if (aclPerm.flags & (fileType == ACL_PERM_FILE ? ACL_PERM_FILE : ACL_PERM_DIR)) == 0 {
                     return
                 }
                 NSLog(aclPerm.name)
             })
 
+            var flagsetT: acl_flagset_t?
+            if acl_get_flagset_np(UnsafeMutableRawPointer.init(entry), &flagsetT) != 0 {
+                continue
+            }
+            aclFlags.forEach({ (aclFlag) in
+                if acl_get_flag_np(flagsetT, aclFlag.flag) == 0 {
+                    return
+                }
+                if (aclFlag.flags & (fileType == ACL_PERM_FILE ? ACL_PERM_FILE : ACL_PERM_DIR)) == 0 {
+                    return
+                }
+                NSLog(aclFlag.name)
+            })
+
+            var maskT: acl_permset_mask_t = UInt64(0)
+            if acl_get_permset_mask_np(entry, &maskT) != 0 {
+                continue
+            }
+            NSLog("\(maskT)")
+
             guard let qualifier = acl_get_qualifier(entry) else {
                 continue
             }
             let udid = qualifier.assumingMemoryBound(to: guid_t.self).pointee
+            var duuid = qualifier.assumingMemoryBound(to: uuid_t.self).pointee
             let uuid = UUID.init(uuid: qualifier.assumingMemoryBound(to: uuid_t.self).pointee)
-
 
             var _user_compat_prefix: uuid_t = (0xff, 0xff, 0xee, 0xee, 0xdd, 0xdd, 0xcc, 0xcc, 0xbb, 0xbb, 0xaa, 0xaa, 0x00, 0x00, 0x00, 0x00)
             var _group_compat_prefix: uuid_t = (0xab, 0xcd, 0xef, 0xab, 0xcd, 0xef, 0xab, 0xcd, 0xef, 0xab, 0xcd, 0xef, 0x00, 0x00, 0x00, 0x00)
 
             if memcmp(qualifier, &_user_compat_prefix, MemoryLayout<uuid_t>.size - MemoryLayout<id_t>.size) == 0 {
+//                var idT: id_t
+//                memcpy(&idT, &duuid[MemoryLayout<uuid_t>.size - MemoryLayout<id_t>.size], MemoryLayout<id_t>.size)
+
                 NSLog("uid")
             } else if memcmp(qualifier, &_group_compat_prefix, MemoryLayout<uuid_t>.size - MemoryLayout<id_t>.size) == 0 {
                 NSLog("gid")
             }
 
-            NSLog("\(tagType) \(uuid)")
+            var uuids = [UInt8].init(repeating: 0, count: 16)
+            memcpy(&uuids, &duuid, MemoryLayout<uuid_t>.size)
 
-//            getpwuid()
-//getgrgid()
-        }
+            if let pw = getpwuuid(&uuids)?.pointee {
+
+            }
+
+            getgruuid(&uuids)
+            
 
 
-        NSLog("\(result)")
-//
+//            NSLog("\(tag) \(uuid) \(pww.pw_name) \(pww.pw_uid) \(pww.pw_gid)")
+        }//
 //        let aclList = SecACLReF
 //
 //
